a426300bb2165538d3c1e0dd5a7e8d1909f12dee
Yaoyao Feng <yfen90@gmail.com>
2026-02-08 20:50:21 +1100

    add

---
+++ tt.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.method, req.url);
+  res.end('Helloss World\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
